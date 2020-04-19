f8fe6666ea1f6dcab3b2ec27dcdda0eaa612a565
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-19 12:24:22 +0530

    Start Up Page

--- app/src/main/java/com/example/salesmartnew/MainActivity.java
+++ app/src/main/java/com/example/salesmartnew/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,5 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
     }
 }
