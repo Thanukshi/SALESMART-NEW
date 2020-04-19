ff2b466eac57ff889149d9b2cd8383ab8e4a1b31
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-19 12:32:50 +0530

    Start Up Page

--- app/src/main/java/com/example/salesmartnew/MainActivity.java
+++ app/src/main/java/com/example/salesmartnew/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  static int SPLASH_SCREEN = 4000;
+    private  static int SPLASH_SCREEN = 5000;
 
     ImageView splashImage;
     Animation topAnim;
