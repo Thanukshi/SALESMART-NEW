51396ab8dbff9504e2b9ac09d9564ad02804ed94
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-19 12:20:55 +0530

    Start Up Page

--- app/src/main/java/com/example/salesmartnew/MainActivity.java
+++ app/src/main/java/com/example/salesmartnew/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.salesmartnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  static int SPLASH_SCREEN = 4000;
+
+    ImageView splashImage;
+    Animation topAnim;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
