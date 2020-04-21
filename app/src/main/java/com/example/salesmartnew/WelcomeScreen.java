be60be0f2d3be7495243588089649509f765d68c
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 14:07:01 +0530

    SaleSmart

---
+++ app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.salesmartnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
