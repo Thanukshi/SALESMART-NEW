4c9fb0ed6a08650e460468889eb9009e50a9158c
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 15:09:57 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/WelcomeViewPageAdaptor.java
+++ app/src/main/java/com/example/salesmartnew/WelcomeViewPageAdaptor.java
@@ -1,4 +1,18 @@
 package com.example.salesmartnew;
 
-public class WelcomeViewPageAdaptor {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class WelcomeViewPageAdaptor extends PagerAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
 }
