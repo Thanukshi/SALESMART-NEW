30f922dcffd9583ffd74c5e08c356970e176cd81
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 15:11:18 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/WelcomeViewPageAdaptor.java
+++ app/src/main/java/com/example/salesmartnew/WelcomeViewPageAdaptor.java
@@ -1,11 +1,19 @@
 package com.example.salesmartnew;
 
+import android.content.Context;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
+import java.util.List;
+
 public class WelcomeViewPageAdaptor extends PagerAdapter {
+
+    Context myContext;
+    List<WelcomeScreen> myListScreen;
+
+
     @Override
     public int getCount() {
         return 0;
