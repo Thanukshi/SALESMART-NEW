139f0a7d7dbf2bb206ca791fd79d9a1b269f052a
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 20:52:17 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
+++ app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
@@ -3,6 +3,7 @@ package com.example.salesmartnew;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -77,7 +78,13 @@ public class WelcomeScreen extends AppCompatActivity {
             }
         });
 
-
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( WelcomeScreen.this, LoginActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 
 }
