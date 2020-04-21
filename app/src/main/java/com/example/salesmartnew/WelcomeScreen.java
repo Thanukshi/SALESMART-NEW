5d8353aba8ef940007086f36068da7dc65b8b898
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 16:08:50 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
+++ app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
@@ -1,16 +1,21 @@
 package com.example.salesmartnew;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class WelcomeScreen extends AppCompatActivity {
 
-
+    private ViewPager screenPage;
+    WelcomeViewPageAdaptor welcomeViewPageAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        screenPage = findViewById(R.id.viewPager_WelcomeScreen);
+        welcomeViewPageAdaptor = new 
     }
 }
