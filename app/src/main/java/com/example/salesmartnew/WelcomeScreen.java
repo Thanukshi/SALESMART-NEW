82f655380c408ff5254322307cfcc254d065de25
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 15:43:26 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
+++ app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
