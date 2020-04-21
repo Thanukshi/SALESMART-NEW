87e9bdeb72f8d3a5cd8f57ab52aefd7a4d7c61d0
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 15:07:44 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/ScreenItems.java
+++ app/src/main/java/com/example/salesmartnew/ScreenItems.java
@@ -4,5 +4,37 @@ public class ScreenItems {
 
     String Tittle, Description;
     int welcomeImage;
-    
+
+    public ScreenItems(String tittle, String description, int welcomeImage) {
+        Tittle = tittle;
+        Description = description;
+        this.welcomeImage = welcomeImage;
+    }
+
+    //generate setters
+    public void setTittle(String tittle) {
+        Tittle = tittle;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+
+    //generate getters
+    public void setWelcomeImage(int welcomeImage) {
+        this.welcomeImage = welcomeImage;
+    }
+
+    public String getTittle() {
+        return Tittle;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public int getWelcomeImage() {
+        return welcomeImage;
+    }
 }
