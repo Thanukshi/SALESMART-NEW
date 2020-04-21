32ffd6ab6ed6e095a845db6f4230e2919b919e3f
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 17:31:30 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
+++ app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
@@ -20,7 +20,7 @@ public class WelcomeScreen extends AppCompatActivity {
 
         //fill list screen
         List<ScreenItems> myList = new ArrayList<>();
-        myList.add(new ScreenItems("Kitchen Equipments", "You can buy new types of kitchen equipments for your kitchen.",R.drawable.onboard_kitchen));
+        myList.add(new ScreenItems("Kitchen Tools", "You can buy new types of kitchen equipments for your kitchen.",R.drawable.onboard_kitchen));
         myList.add(new ScreenItems("Fast Delivery", "You can buy new types of kitchen equipments for your kitchen.",R.drawable.onboard_kitchen5));
         myList.add(new ScreenItems("Easy Payments", "You can buy new types of kitchen equipments for your kitchen.",R.drawable.onboard_kitchen4));
 
