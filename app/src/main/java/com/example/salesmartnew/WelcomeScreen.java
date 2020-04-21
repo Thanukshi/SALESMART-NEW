bb96ba527f213569b1b364011ede4206c15e895d
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-21 19:35:31 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
+++ app/src/main/java/com/example/salesmartnew/WelcomeScreen.java
@@ -6,6 +6,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -17,6 +18,7 @@ public class WelcomeScreen extends AppCompatActivity {
     private ViewPager screenPage;
     WelcomeViewPageAdaptor welcomeViewPageAdaptor;
     TabLayout tabIndicator;
+    ImageView btnNxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
