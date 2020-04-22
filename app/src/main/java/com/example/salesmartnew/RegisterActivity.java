844bf4155743bfc435cf033738ca4a7cfbe50757
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 16:38:57 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -34,8 +34,14 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //Initialize the view
         RL1 = findViewById(R.id.RL1_Register);
         bacKArrow = findViewById(R.id.image1_Register);
+        rFullName = findViewById(R.id.ET1_Register);
+        rUserName = findViewById(R.id.ET2_Register);
+        rPassword = findViewById(R.id.ET3_Register);
+        rConfirmPass = findViewById(R.id.ET4_Register);
+        rAddPhoto = findViewById(R.id.addImage_Register);
 
         handler.postDelayed(runnable, 1000);
 
@@ -54,6 +60,14 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intentRegister = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intentRegister);
+            }
+        });
+
+
+        rAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get the runtime permission from the phone 
             }
         });
     }
