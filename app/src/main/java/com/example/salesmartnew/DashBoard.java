21701f8a2014b4a3d80a863dad8d2eab2aeabf73
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 14:55:14 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/DashBoard.java
+++ app/src/main/java/com/example/salesmartnew/DashBoard.java
@@ -1,14 +1,34 @@
 package com.example.salesmartnew;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class DashBoard extends AppCompatActivity {
 
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private  DashBoardFragmentViewPagerAdapter dashBoardFragmentViewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayOut_Dash);
+        viewPager = (ViewPager) findViewById(R.id.viewPager_Dash);
+        dashBoardFragmentViewPagerAdapter = new DashBoardFragmentViewPagerAdapter(getSupportFragmentManager());
+
+
+        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentHome(), "Home");
+        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentMessage(), "Message");
+        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentCart(), "Cart");
+        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentAccount(), "Account");
+
+        viewPager.setAdapter(dashBoardFragmentViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/com/example/salesmartnew/DashBoardFragmentViewPagerAdapter.java
+++ app/src/main/java/com/example/salesmartnew/DashBoardFragmentViewPagerAdapter.java
@@ -1,4 +1,45 @@
 package com.example.salesmartnew;
 
-public class DashBoardFragmentViewPagerAdapter {
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DashBoardFragmentViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment>lstFragment = new ArrayList<>();
+    private final List<String>lstTittle = new ArrayList<>();
+
+    public DashBoardFragmentViewPagerAdapter(FragmentManager fm) {
+       super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return lstFragment.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return lstTittle.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return lstTittle.get(position);
+    }
+
+    public void AddFragment (Fragment fragment, String Title){
+        lstFragment.add(fragment);
+        lstTittle.add(Title);
+    }
 }
