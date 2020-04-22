5a9cc7b971be2f90b9b5131ebae4f139fd7f5c1b
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 14:23:40 +0530

    SaleSmart

---
+++ app/src/main/java/com/example/salesmartnew/DashBoardFragmentHome.java
@@ -0,0 +1,24 @@
+package com.example.salesmartnew;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DashBoardFragmentHome extends Fragment {
+
+    View homeView;
+
+    public DashBoardFragmentHome() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        homeView = inflater.inflate(R.layout.f);
+    }
+}
