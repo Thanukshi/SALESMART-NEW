2842e6b2a76821da3b6deb79adf97ae345f0e2b2
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 12:09:10 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.salesmartnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -11,9 +14,32 @@ public class RegisterActivity extends AppCompatActivity {
     Button btRegister;
     RelativeLayout RL1;
 
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+
+            RL1.setVisibility(View.VISIBLE);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        RL1 = findViewById(R.id.RL1_Register);
+
+        handler.postDelayed(runnable, 1000);
+
+        btRegister = findViewById(R.id.button1_Register);
+
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegister = new Intent(RegisterActivity.this, DashBoard.class);
+                startActivity(intentRegister);
+            }
+        });
     }
 }
