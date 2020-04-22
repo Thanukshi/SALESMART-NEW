1a5455c3751bab32c02115551c6281ba8cbfb6ef
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 16:33:14 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText rFullName, rUserName, rPassword, rConfirmPass;
+    ImageView rAddPhoto;
     Button btRegister;
     RelativeLayout RL1;
     ImageView bacKArrow;
