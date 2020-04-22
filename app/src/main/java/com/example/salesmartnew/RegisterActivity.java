6ecbce4eeca6366ed12eb838bfb35ffefea64620
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 16:55:29 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -1,9 +1,13 @@
 package com.example.salesmartnew;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -11,8 +15,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    final int REQUEST_CODE_GALLERY = 999;
 
     EditText rFullName, rUserName, rPassword, rConfirmPass;
     ImageView rAddPhoto;
@@ -69,8 +76,34 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //get the runtime permission from the phone
-                ActivityCompat.requestPermissions();
+                ActivityCompat.requestPermissions(
+                        RegisterActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_GALLERY);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,REQUEST_CODE_GALLERY);
+            }
+            else {
+                Toast.makeText(this,"Don't have to permition to access the file location", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == REQUEST_CODE_GALLERY &&  resultCode == RESULT_OK){
+            
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
