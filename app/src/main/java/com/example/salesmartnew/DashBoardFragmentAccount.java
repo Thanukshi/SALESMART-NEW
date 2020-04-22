6b5d85c8c0e66681a9e4dcd8dc0fe40e4065b61f
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 14:34:08 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/DashBoardFragmentAccount.java
+++ app/src/main/java/com/example/salesmartnew/DashBoardFragmentAccount.java
@@ -1,4 +1,25 @@
 package com.example.salesmartnew;
 
-public class DashBoardFragmentAccount {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DashBoardFragmentAccount extends Fragment {
+
+    View accountView;
+
+    public DashBoardFragmentAccount() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        accountView = inflater.inflate(R.layout.fragment_account, container, false);
+        return  accountView;
+    }
 }
