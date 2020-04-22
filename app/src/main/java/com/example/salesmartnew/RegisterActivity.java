49af4cb17a6d6e89da0f44b51ef6b9a632675cf0
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 12:20:57 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Button btRegister;
     RelativeLayout RL1;
+    ImageView bacKArrow;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -20,6 +22,7 @@ public class RegisterActivity extends AppCompatActivity {
         public void run() {
 
             RL1.setVisibility(View.VISIBLE);
+            bacKArrow.setVisibility(View.VISIBLE);
         }
     };
 
@@ -29,15 +32,24 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         RL1 = findViewById(R.id.RL1_Register);
+        bacKArrow = findViewById(R.id.image1_Register);
 
         handler.postDelayed(runnable, 1000);
+
+        bacKArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(goBack);
+            }
+        });
 
         btRegister = findViewById(R.id.button1_Register);
 
         btRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentRegister = new Intent(RegisterActivity.this, DashBoard.class);
+                Intent intentRegister = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intentRegister);
             }
         });
