c4ba368dc6d680d93953a15e40b1a2a2d19b9e45
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 14:29:42 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/DashBoardFragmentMessage.java
+++ app/src/main/java/com/example/salesmartnew/DashBoardFragmentMessage.java
@@ -1,4 +1,26 @@
 package com.example.salesmartnew;
 
-public class DashBoardFragmentMessage {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DashBoardFragmentMessage extends Fragment {
+
+    View messageView;
+
+
+    public DashBoardFragmentMessage() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        messageView = inflater.inflate(R.layout.fragment_message, container,false);
+        return messageView;
+    }
 }
