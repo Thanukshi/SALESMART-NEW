f97dd87982cf4941ca38b9dda9ef6f22d0fb11a6
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-22 12:05:12 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -3,8 +3,13 @@ package com.example.salesmartnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btRegister;
+    RelativeLayout RL1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
