cdc57be06bc86096b80b0de6170e467ed4d7c181
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-23 15:47:53 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -2,10 +2,19 @@ package com.example.salesmartnew;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ViewPager regScreenPage;
+    TabLayout tabLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
