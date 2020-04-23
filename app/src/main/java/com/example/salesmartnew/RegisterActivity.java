09095aa6e9070f568fc139d997a42fa8005e42cf
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-23 11:37:46 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterActivity.java
+++ app/src/main/java/com/example/salesmartnew/RegisterActivity.java
@@ -1,18 +1,16 @@
 package com.example.salesmartnew;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 public class RegisterActivity extends AppCompatActivity {
-
-       @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
-            }
-        }
+    }
 
 }
