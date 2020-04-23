abb487ce61a657ddc736f5884066cb05b25fa533
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-23 15:32:32 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterScreenItem.java
+++ app/src/main/java/com/example/salesmartnew/RegisterScreenItem.java
@@ -2,5 +2,17 @@ package com.example.salesmartnew;
 
 public class RegisterScreenItem {
 
-    
+    int addPhoto;
+
+    public RegisterScreenItem(int addPhoto) {
+        this.addPhoto = addPhoto;
+    }
+
+    public void setAddPhoto(int addPhoto) {
+        this.addPhoto = addPhoto;
+    }
+
+    public int getAddPhoto() {
+        return addPhoto;
+    }
 }
