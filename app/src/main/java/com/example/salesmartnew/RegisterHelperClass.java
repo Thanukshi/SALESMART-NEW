1fc4cca03af95e4ffb30526b0e97a7b87ba2b920
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-24 16:26:33 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/RegisterHelperClass.java
+++ app/src/main/java/com/example/salesmartnew/RegisterHelperClass.java
@@ -1,4 +1,57 @@
 package com.example.salesmartnew;
 
 public class RegisterHelperClass {
+
+    private String fullName;
+
+    private String emailCustomer;
+
+    private String userNameCustomer;
+
+    private String passwordCustomer;
+
+    private String confirmPasswordCustomer;
+
+    public RegisterHelperClass() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmailCustomer() {
+        return emailCustomer;
+    }
+
+    public String getUserNameCustomer() {
+        return userNameCustomer;
+    }
+
+    public String getPasswordCustomer() {
+        return passwordCustomer;
+    }
+
+    public String getConfirmPasswordCustomer() {
+        return confirmPasswordCustomer;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmailCustomer(String emailCustomer) {
+        this.emailCustomer = emailCustomer;
+    }
+
+    public void setUserNameCustomer(String userNameCustomer) {
+        this.userNameCustomer = userNameCustomer;
+    }
+
+    public void setPasswordCustomer(String passwordCustomer) {
+        this.passwordCustomer = passwordCustomer;
+    }
+
+    public void setConfirmPasswordCustomer(String confirmPasswordCustomer) {
+        this.confirmPasswordCustomer = confirmPasswordCustomer;
+    }
 }
