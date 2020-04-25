6f03ee9a26ecf1df03f4702ac5ff92b6a08f6b6f
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-25 12:16:13 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/LoginActivity.java
+++ app/src/main/java/com/example/salesmartnew/LoginActivity.java
@@ -127,11 +127,7 @@ public class LoginActivity extends AppCompatActivity {
 
                             String pwInDB =dataSnapshot.child(un).child("passwordCustomer").getValue(String.class);
                             String unIDb = dataSnapshot.child(un).child("userNameCustomer").getValue(String.class);
-                            if(unIDb.equals("admin") || pwInDB.equals("Admin@1234")){
-                                Intent adminIntent = new Intent(getApplicationContext(),AdminView.class);
-                                startActivity(adminIntent);
-                            }
-                           if(pwInDB.equals(pw)){
+                            if(pwInDB.equals(pw)){
 
                                 Intent logIntent = new Intent(getApplicationContext(),DashBoard.class);
                                 startActivity(logIntent);
