e9ea712995df3bf760dded1641576ed1d3a5b4d2
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-25 14:24:43 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/LoginActivity.java
+++ app/src/main/java/com/example/salesmartnew/LoginActivity.java
@@ -22,7 +22,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -54,6 +60,18 @@ public class LoginActivity extends AppCompatActivity {
     };
     private GoogleSignInClient mGoogleSignInClient;
     private final static int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null){
+            Intent googleIntent = new Intent(getApplicationContext(),DashBoard.class);
+            startActivity(googleIntent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,6 +118,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         createRequest();
+        mAuth = FirebaseAuth.getInstance();
 
         btnLogin.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -201,7 +220,29 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if(user != null){
+                                Intent googleIntent = new Intent(getApplicationContext(),DashBoard.class);
+                                startActivity(googleIntent);
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(),"Sorry your authentication is failed.",Toast.LENGTH_SHORT).show();
+                            
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
