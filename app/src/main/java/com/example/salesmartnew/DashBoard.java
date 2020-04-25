7803755a5856ceed92e22aa709cdbf282fc8d5b9
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-25 22:26:47 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/DashBoard.java
+++ app/src/main/java/com/example/salesmartnew/DashBoard.java
@@ -11,31 +11,11 @@ import com.google.android.material.tabs.TabLayout;
 public class DashBoard extends AppCompatActivity {
 
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private  DashBoardFragmentViewPagerAdapter dashBoardFragmentViewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
-
-        tabLayout = (TabLayout) findViewById(R.id.tabLayOut_Dash);
-        viewPager = (ViewPager) findViewById(R.id.viewPager_Dash);
-        dashBoardFragmentViewPagerAdapter = new DashBoardFragmentViewPagerAdapter(getSupportFragmentManager());
-
-
-        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentHome(), "Home");
-        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentMessage(), "Message");
-        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentCart(), "Cart");
-        dashBoardFragmentViewPagerAdapter.AddFragment(new DashBoardFragmentAccount(), "Account");
-
-        viewPager.setAdapter(dashBoardFragmentViewPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_message);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_shopping_cart);
-        tabLayout.getTabAt(3).setIcon(R.drawable.ic_person);
+        
 
 
     }
