5c3b5a76a931b90bcc6adc4f0f3b98f91e48de86
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-26 14:30:42 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/DashBoard.java
+++ app/src/main/java/com/example/salesmartnew/DashBoard.java
@@ -3,11 +3,13 @@ package com.example.salesmartnew;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
@@ -34,14 +36,39 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
         drawerLayout = findViewById(R.id.RL1_Dash);
         navigationView = findViewById(R.id.nav);
 
+
+
+        navigationDrawer();
+
+    }
+
+    //Navigation Drawer Functions
+
+    private void navigationDrawer() {
         //navigation drawer
         navigationView.bringToFront();
         navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
         navigationView.setCheckedItem(R.id.db1);
 
-
+        drawerMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(drawerLayout.isDrawerVisible(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
     }
-
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return true;
