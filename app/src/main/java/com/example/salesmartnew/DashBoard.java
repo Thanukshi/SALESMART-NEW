52ae356de82071c16c5d3959997a1a855c74397e
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-26 13:37:20 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/DashBoard.java
+++ app/src/main/java/com/example/salesmartnew/DashBoard.java
@@ -2,20 +2,29 @@ package com.example.salesmartnew;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class DashBoard extends AppCompatActivity {
 
+
+    //Drawer Menu Bar
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
         
+        //Hooks for the navigation
+        drawerLayout = findViewById(R.id.RL1_Dash);
+        navigationView = findViewById(R.id.nav);
 
 
     }
