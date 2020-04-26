625ebde1b9f285004ed35d816c7d818b0f5f6872
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-26 14:18:36 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/DashBoard.java
+++ app/src/main/java/com/example/salesmartnew/DashBoard.java
@@ -8,6 +8,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
@@ -15,15 +16,20 @@ import com.google.android.material.tabs.TabLayout;
 public class DashBoard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
+    ImageView drawerMenu;
+
     //Drawer Menu Bar
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
-        
+
+        drawerMenu = findViewById(R.id.menuIcon_Dash);
+
         //Hooks for the navigation
         drawerLayout = findViewById(R.id.RL1_Dash);
         navigationView = findViewById(R.id.nav);
