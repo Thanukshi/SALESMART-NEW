116ae417418a15a23a1d54eec4038c40961f46ba
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-27 20:43:16 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/Profile.java
+++ app/src/main/java/com/example/salesmartnew/Profile.java
@@ -3,12 +3,31 @@ package com.example.salesmartnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 
 public class Profile extends AppCompatActivity {
+
+    TextView userNameText;
+    TextView fullNameText;
+    TextView emailText;
+    TextView passwordText;
+    TextView contactText;
+    ImageView profImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        userNameText = findViewById(R.id.profText1);
+        fullNameText = findViewById(R.id.text1__ProfLL);
+        emailText = findViewById(R.id.text2__ProfLL);
+        passwordText = findViewById(R.id.text3__ProfLL);
+        contactText = findViewById(R.id.text4__ProfLL);
+        profImage = findViewById(R.id.profImage);
+
     }
 }
