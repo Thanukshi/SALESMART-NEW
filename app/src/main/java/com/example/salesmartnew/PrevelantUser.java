0321153e6d65676e6df3b0dcfa1fc95774ce6254
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-28 20:36:17 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/PrevelantUser.java
+++ app/src/main/java/com/example/salesmartnew/PrevelantUser.java
@@ -1,4 +1,10 @@
 package com.example.salesmartnew;
 
 public class PrevelantUser {
+
+    public static RegisterHelperClass currentUser;
+
+    public static final String PhoneId = "contactNo";
+    public static final String PasswordId = "passwordCustomer";
+
 }
