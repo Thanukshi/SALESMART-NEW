1e3c6f51bf252fa1280922d14ee8705437006832
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 02:18:30 +0530

    IT18184518

--- app/src/main/java/com/example/salesmartnew/SettingsActivity.java
+++ app/src/main/java/com/example/salesmartnew/SettingsActivity.java
@@ -3,12 +3,17 @@ package com.example.salesmartnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+    TextView textViewDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
     }
 }
