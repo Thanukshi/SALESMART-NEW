e6a89f303e6e28155258883b469552b00dd343f3
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-28 10:35:15 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/EditProfileActivity.java
+++ app/src/main/java/com/example/salesmartnew/EditProfileActivity.java
@@ -3,12 +3,21 @@ package com.example.salesmartnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    EditText uUserName, uFullName, uEmail, uPassword, uConPass, uContact;
+    ImageView uProfImage;
+    Button update, delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        uUserName = findViewById(R.id.text1_EP);
     }
 }
