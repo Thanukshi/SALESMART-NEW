989b60c3401a50a0c9ddfe4ba3a557b6782765dc
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-28 11:00:33 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/EditProfileActivity.java
+++ app/src/main/java/com/example/salesmartnew/EditProfileActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     EditText uUserName, uFullName, uEmail, uPassword, uConPass, uContact;
     ImageView uProfImage;
     Button update, delete;
+    DatabaseReference dbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,13 @@ public class EditProfileActivity extends AppCompatActivity {
         uFullName.setText(fullNameEdit);
         uEmail.setText(emailEdit);
         uPassword.setText(passwordEdit);
+
+        dbReference = FirebaseDatabase.getInstance().getReference("users");
+
+    }
+
+    public void update(View view){
+        
 
     }
 }
