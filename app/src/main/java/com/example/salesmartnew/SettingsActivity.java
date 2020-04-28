08823c8e64648a3a37569659a9b972501cbe8183
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-29 02:28:15 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/SettingsActivity.java
+++ app/src/main/java/com/example/salesmartnew/SettingsActivity.java
@@ -2,18 +2,36 @@ package com.example.salesmartnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SettingsActivity extends AppCompatActivity {
     TextView textViewDelete;
+    DatabaseReference dbf;
+    String contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        textViewDelete = findViewById(R.id.textViewDelete);
+        Intent intent = getIntent();
+        contact= intent.getStringExtra("contactNo");
 
+        textViewDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbf = FirebaseDatabase.getInstance().getReference().child("users").child(contact);
+                dbf.removeValue();
+                startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
+            }
+        });
 
     }
 }
