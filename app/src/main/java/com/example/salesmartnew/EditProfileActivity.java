08a8f5525b0d2d6c50695461a7d8ee05a703dedb
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-28 20:05:36 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/EditProfileActivity.java
+++ app/src/main/java/com/example/salesmartnew/EditProfileActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 
@@ -17,13 +18,13 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-   private CircleImageView profImage;
-   private EditText fullNameUp, contactUp, emailUp, passwordUp, confirmPassUp;
+   CircleImageView profImage;
+   EditText fullNameUp, contactUp, emailUp, passwordUp, confirmPassUp;
    Button updateProf, closeProf;
 
    private Uri imageUri;
    private String myUrl = "";
-   private DatabaseReference storeProfImage;
+   private StorageReference storeProfImage;
    private String checker = "";
    private StorageTask uploadTask;
 
@@ -33,7 +34,17 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        storeProfImage = FirebaseDatabase.getInstance().getReference().child("image");
+        storeProfImage = FirebaseStorage.getInstance().getReference().child("image");
+
+        //value assignment
+        profImage = (CircleImageView)findViewById(R.id.EPImage);
+        fullNameUp = (EditText)findViewById(R.id.text1_EP);
+        contactUp = (EditText)findViewById(R.id.text2_EP);
+        emailUp =  (EditText)findViewById(R.id.text3_EP);
+        passwordUp = (EditText)findViewById(R.id.text4_EP);
+        confirmPassUp = (EditText)findViewById(R.id.text5_EP);
+        
+
 
 
 
