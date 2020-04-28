9557bf32217905c998ad41fd916b421a58d0a301
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-28 09:32:06 +0530

    SaleSmart

---
+++ app/src/main/java/com/example/salesmartnew/EditProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.salesmartnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile2);
+    }
+}
