b827aa5cf3dc395f473e9517d0a64c8b9fd7e173
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 19:23:58 +0530

    IT18184518

---
+++ app/src/main/java/com/example/salesmart/delivery/Delivery_Admin.java
@@ -0,0 +1,47 @@
+package com.example.salesmart.delivery;
+import com.example.salesmart.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//import com.salesmart.salesmart.R;
+
+public class Delivery_Admin extends AppCompatActivity {
+    Button buttonInsert,buttonSearch,buttonListAll;
+    String username = "Default";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery__admin);
+        buttonInsert = findViewById(R.id.buttonAdmin);
+        buttonSearch = findViewById(R.id.buttonCustomer);
+        buttonListAll = findViewById(R.id.buttonListAll);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Delivery_Admin.this, insertdelivery.class);
+                intent.putExtra("username", username);
+                startActivity(intent);
+            }
+        });
+
+        buttonListAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Delivery_Admin.this, ListAll.class);
+                startActivity(intent);
+            }
+        });
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Delivery_Admin.this, aview_Delivery.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
