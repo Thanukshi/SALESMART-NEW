b579129066695c83abddf0ae8401df2dd0932082
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 22:05:24 +0530

    IT18184518 Salesmart Intergrate

--- app/src/main/java/com/example/salesmart/delivery/ListAllCustomer.java
+++ app/src/main/java/com/example/salesmart/delivery/ListAllCustomer.java
@@ -1,5 +1,5 @@
 package com.example.salesmart.delivery;
-import com.example.salesmart.R;
+import com.example.salesmartn.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
