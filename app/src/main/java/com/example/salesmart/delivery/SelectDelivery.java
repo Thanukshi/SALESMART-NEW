db46c7b43f32b2df01b1761e0a5ad40d43c64439
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 22:08:03 +0530

    IT18184518 Salesmart Intergrate

--- app/src/main/java/com/example/salesmart/delivery/SelectDelivery.java
+++ app/src/main/java/com/example/salesmart/delivery/SelectDelivery.java
@@ -1,5 +1,5 @@
 package com.example.salesmart.delivery;
-import com.example.salesmart.R;
+import com.example.salesmartn.R;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
