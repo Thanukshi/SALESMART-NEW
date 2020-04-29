75de8af82559e3b7a3693d042faa88dcbbc1394a
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 21:52:59 +0530

    IT18184518 Main Rename

--- app/src/main/java/com/example/salesmart/delivery/MainActivity.java
+++ app/src/main/java/com/example/salesmart/delivery/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_M);
 
         buttonAdmin = findViewById(R.id.buttonAdmin);
         buttonCustomer = findViewById(R.id.buttonCustomer);
