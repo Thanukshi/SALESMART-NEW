3b4918f8100fb711739224c3740e8f9c3ea5834f
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 22:07:22 +0530

    IT18184518 Salesmart Intergrate

--- app/src/main/java/com/example/salesmart/delivery/MainActivity23.java
+++ app/src/main/java/com/example/salesmart/delivery/MainActivity23.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.salesmart.R;
+import com.example.salesmartn.R;
 
 public class MainActivity23 extends AppCompatActivity {
 
