f631db815b937194dfb602324901339662c753ed
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 22:04:57 +0530

    IT18184518 Salesmart Intergrate

--- app/src/main/java/com/example/salesmart/delivery/ListAll.java
+++ app/src/main/java/com/example/salesmart/delivery/ListAll.java
@@ -1,5 +1,5 @@
 package com.example.salesmart.delivery;
-import com.example.salesmartn.R;
+import com.example.salesmartne.R;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
