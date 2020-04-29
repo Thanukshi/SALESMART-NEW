d994651242398342f2dfa280278fbe85f561109a
Kavindi Gayaththri <kavindigayaththri@gmail.com>
2020-04-29 22:18:38 +0530

    IT18184518 Salesmart Intergrate

--- app/src/main/java/com/example/salesmart/delivery/cview_Delivery.java
+++ app/src/main/java/com/example/salesmart/delivery/cview_Delivery.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-//impo
+//imp
 
 
 public class cview_Delivery extends AppCompatActivity {
