e8e3d773cd61306d07a8c798c5a7946821ba9087
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-04-30 14:12:55 +0530

    SaleSmart

--- app/src/main/java/com/example/salesmartnew/HomeAdapterHelperClass/EquipmentAdapter.java
+++ app/src/main/java/com/example/salesmartnew/HomeAdapterHelperClass/EquipmentAdapter.java
@@ -1,4 +1,66 @@
 package com.example.salesmartnew.HomeAdapterHelperClass;
 
-public class EquipmentAdapter {
+
+
+
+import android.animation.PointFEvaluator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.salesmartnew.R;
+
+import java.util.ArrayList;
+
+public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.EquipmentViewHolder> {
+
+    ArrayList<EquipmentHelperClass> equipmentHelperClasses ;
+
+    public EquipmentAdapter(ArrayList<EquipmentHelperClass> equipmentHelperClasses) {
+        this.equipmentHelperClasses = equipmentHelperClasses;
+    }
+
+    @NonNull
+    @Override
+    public EquipmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.equipment_design, parent, false);
+        EquipmentViewHolder equipmentViewHolder =  new EquipmentViewHolder(view);
+        return equipmentViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EquipmentViewHolder holder, int position) {
+        EquipmentHelperClass equipmentHelperClass = equipmentHelperClasses.get(position);
+        holder.image.setImageResource(equipmentHelperClass.getImage());
+        holder.title.setText(equipmentHelperClass.getTitleCard());
+        holder.desc.setText(equipmentHelperClass.getDescriptionCard());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class EquipmentViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView title, desc;
+
+        public EquipmentViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //identify the layout
+            image = itemView.findViewById(R.id.image_card);
+            title = itemView.findViewById(R.id.text1_card);
+            desc = itemView.findViewById(R.id.text2_card);
+        }
+    }
+
+
 }
